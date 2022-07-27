9d6830aac9ecc0858a15487c23d38e85b0cd4db7
Sam Freeman <63522296+sam-c-freeman@users.noreply.github.com>
2022-07-27 10:38:44 -0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,28 @@
+// function returner(){
+//     console.log('in the returner function');
+//     return true;
+// }
+
+// returner();
+// console.log('running returner function:', returner());
+
+// function mathExample(){
+//     console.log('in mathExample function');
+//     let answer = 3*9;
+//     return answer;
+// }
+
+// console.log('running mathExample:', mathExample());
+
+// function actualMath(num0, num1){
+//     console.log('in actualMath function', num0, num1);
+//     let answer = num0 * num1;
+//     return answer;
+
+// }
+
+// console.log('running actualMath with 4 & 6:', actualMath(4,6));
+// console.log('running actualMath with 1 & 6:', actualMath(1,6));
+// console.log('running actualMath with 4 & 3:', actualMath(4,3));
+
+
